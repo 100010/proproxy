12810f25f49d278d914dd92c2ec5a497862045d7
100010 <yohei.senju@gmail.com>
2018-09-20 08:42:05 +0900

    Add command line

--- lib/proproxy.rb
+++ lib/proproxy.rb
@@ -1,5 +1,26 @@
 require "proproxy/version"
+require "proproxy/cli"
+require 'pry'
 
 module Proproxy
-  # Your code goes here...
+  class CLI < ::Thor
+    desc 'provision proxy server'
+    def set_up(server_name)
+      # install squid, and apt-get update
+    end
+
+    desc 'add proxy server ip with port'
+    def add_ip(server_name, ip, port)
+      # edit iptables and save
+      # configure ip tables
+      # restart squid
+    end
+
+    desc 'remove specified ip from proxy server'
+    def remove_ip(server_name, ip, port)
+      # edit iptables and save
+      # configure ip tables
+      # restart squid
+    end
+  end
 end
